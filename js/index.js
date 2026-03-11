f9a611c943be7227bb86e0bd87deb39f08dd9bf6
Teemu <willniko67@gmail.com>
2026-03-11 14:55:40 +0200

    part 4

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const BACKEND_ROOT_URL = 'http://localhost:3001'
+import { Todos } from "./class/Todos.js"
+
+const todos = new Todos(BACKEND_ROOT_URL)
 
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
@@ -8,36 +11,19 @@ const input = document.querySelector('input')
 const renderTask = (task)=>{
     const li = document.createElement('li')
     li.setAttribute('class', 'list-group-item')
-    li.innerHTML = task;
+    li.innerHTML = task.getText();
     list.append(li)
 }
 
 const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL)
-        const json = await response.json()
-        json.forEach(task => {
-            renderTask(task.description)
+    todos.getTasks().then((tasks)=>{
+        tasks.forEach(task => {
+            renderTask(task)
         });
-    } catch (error) {
-        alert("Error receiving tasks "+error.message)
-    }
-}
-
-const saveTask = async (task) => {
-    try {
-        const json = JSON.stringify({description:task})
-        const response = await fetch(BACKEND_ROOT_URL+ '/new', {
-            method: 'post',
-            headers: {
-                'Content-Type':'application/json'
-            },
-            body: json
-        })
-        return  response.json()
-    } catch (error) {
-        alert("Error saving task "+error.message)
-    }
+        input.disabled = false
+    }).catch((error)=>{
+        alert(error)
+    })
 }
 
 getTasks()
@@ -47,9 +33,10 @@ input.addEventListener('keypress', (event)=>{
         event.preventDefault()
         const task = input.value.trim()
         if (task !== ''){
-            saveTask(task).then((json)=>{
+            todos.addTask(task).then((task)=>{
                 renderTask(task);
                 input.value = ''
+                input.focus()
             })
         }
     }
