c594e16d63fe0feca4c7c0420d0acff24c67b70c
Teemu <willniko67@gmail.com>
2026-03-13 12:26:43 +0200

    part 7

--- server/index.js
+++ server/index.js
@@ -1,56 +1,15 @@
 require('dotenv').config()
-console.log(process.env)
 const express = require('express');
 const cors = require('cors');
-// const { Pool } = require('pg')
-const { query } = require('./helpers/db.js')
+const { todoRouter } = require('./routes/todo.js')
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+app.use('/', todoRouter)
 
 const port = process.env.PORT;
-
-app.get("/",async (req,res)=>{
-    console.log(query)
-    try {
-        const result = await query('select * from task')
-        const rows = result.rows ? result.rows : [];
-        res.status(200).json(rows)
-    } catch (error) {
-        console.log(error)
-        res.statusMessage = error
-        res.status(500).json({error: error})
-    }
-});
-
-app.post("/new", async(req,res)=>{
-    
-try{
-    const result = await query('insert into task (description) values ($1) returning *',
-    [req.body.description])
-    res.status(200).json({id:result.rows[0].id})        
-    } catch (error) {
-        console.log(error)
-        res.statusMessage = error
-        res.status(500).json({error: error})
-    }
-});
-
-app.delete("/delete/:id", async(req,res)=>{
-    
-    const id = parseInt(req.params.id)
-    try {
-        const result = await query('DELETE FROM task WHERE id = $1',
-        [id])
-        res.status(200).json({id:id})
-    } catch(error){
-        console.log(error)
-        res.statusMessage = error
-        re.status(500).json({error: error})
-    }
-})
 
 app.listen(port);
