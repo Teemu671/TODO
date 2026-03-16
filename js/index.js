7d227c05ab35494df4afbdbea30ff231578c0fdd
Teemu <willniko67@gmail.com>
2026-03-16 10:13:48 +0200

    Adding backend

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001'
+//const BACKEND_ROOT_URL = 'http://localhost:3001'
+const BACKEND_ROOT_URL = 'https://todo-backend-yka8.onrender.com'
+
 import { Todos } from "./class/Todos.js"
 
 const todos = new Todos(BACKEND_ROOT_URL)
